847d65b8e7929444523093aeee416b150be7a91b
abatisse <abatisse@hevaweb.com>
2022-08-19 09:00:53 +0200

    ci: Update actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,13 @@ import {defineNuxtConfig} from 'nuxt'
 export default defineNuxtConfig({
     ssr: false,
     modules: ['@nuxtjs/tailwindcss'],
+    hooks: {
+        'vite:extendConfig'(config, {isClient}) {
+            if (process.env.NODE_ENV !== 'development' && isClient) {
+                config.build.rollupOptions.output.chunkFileNames = '_nuxt/[hash].js'
+                config.build.rollupOptions.output.entryFileNames = '_nuxt/entry.[hash].js'
+            }
+        }
+    },
+
 })
